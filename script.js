e73a16c3db6ac6ff7d9911ac9a361d9438bed8db
DyandraWI <dyandra.wahyu@gmail.com>
2023-11-24 19:42:09 +0700

    Percobaan pertama

---
+++ script.js
@@ -0,0 +1,27 @@
+
+// let sections = document.querySelectorAll('section');
+// let navLinks = document.querySelectorAll('.nav-link');
+
+// window.onscroll = () => {
+//     sections.forEach(sec => {
+//         let top = window.scrollY;
+//         let offset = sec.offsetTop - 150;
+//         let height = sec.offsetHeight;
+//         let id = sec.getAttribute('id');
+        
+//         if(top >= offset && top < offset + height){
+//             navLinks    .forEach(links => {
+//                 links.classList.remove('active');
+//                 document.querySelector('.nav-link[href*=' + id + ']').classList.add('active');
+//             });
+//         };
+//     });
+//     Sticky
+//     let nav = document.querySelector('nav');
+
+//     nav.classList.toggle('sticky', window.scrollY > 100);
+
+//     menuIcon.classList.remove('navbar-toggler-icon');
+//     navbar.classList.remove('active');
+// };
+  
