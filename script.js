8bebc666cce588667e789c71f917e94bf5626b4d
DyandraWI <dyandra.wahyu@gmail.com>
2023-11-26 02:39:12 +0700

    First Try

--- script.js
+++ script.js
@@ -25,3 +25,17 @@
 //     navbar.classList.remove('active');
 // };
   
+
+const hamburger = document.querySelector(".hamburger");
+const navmenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navmenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(n => 
+    n.addEventListener("click", () => {
+        hamburger.classList.remove("active");
+        navmenu.classList.remove("active");
+    }))
